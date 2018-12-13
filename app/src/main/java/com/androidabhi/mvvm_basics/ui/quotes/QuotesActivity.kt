ebc16f548751ad65fd7941c37af6996c1232d864
chauhan-abhi <chauhanabhi1245@gmail.com>
2018-12-13 20:43:00 +0530

    initial commit

---
+++ app/src/main/java/com/androidabhi/mvvm_basics/ui/quotes/QuotesActivity.kt
@@ -0,0 +1,13 @@
+package com.androidabhi.mvvm_basics.ui.quotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androidabhi.mvvm_basics.R
+
+class QuotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
