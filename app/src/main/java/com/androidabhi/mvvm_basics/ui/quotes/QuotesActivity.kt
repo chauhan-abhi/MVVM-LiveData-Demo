baa8ad65ce332db920b3740bbc6e7bf1d3c9f05a
chauhan-abhi <chauhanabhi1245@gmail.com>
2018-12-14 15:20:38 +0530

    adding and retrieving quotes working

--- app/src/main/java/com/androidabhi/mvvm_basics/ui/quotes/QuotesActivity.kt
+++ app/src/main/java/com/androidabhi/mvvm_basics/ui/quotes/QuotesActivity.kt
@@ -2,12 +2,45 @@ package com.androidabhi.mvvm_basics.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.androidabhi.mvvm_basics.R
+import com.androidabhi.mvvm_basics.data.Quote
+import com.androidabhi.mvvm_basics.utilities.InjectorsUtils
+import kotlinx.android.synthetic.main.activity_quotes.*
+import java.lang.StringBuilder
 
 class QuotesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quotes)
+        initializeUi()
+    }
+
+    private fun initializeUi() {
+        // Get the QuotesViewModelFactory with all of it's dependencies constructed
+        val factory = InjectorsUtils.providesQuotesViewModelFactory()
+        // Use ViewModelProviders class to create / get already created QuotesViewModel
+        // for this view (activity)
+        val viewModel = ViewModelProviders.of(this, factory)
+            .get(QuotesViewModel::class.java)
+
+        // Observing LiveData from the QuotesViewModel which in turn observes
+        // LiveData from the repository, which observes LiveData from the DAO
+        viewModel.getQuotes().observe(this, Observer { quotes ->
+            val stringBuilder = StringBuilder()
+            quotes.forEach {quote ->
+                stringBuilder.append("$quote \n\n")
+            }
+            textView_quotes.text = stringBuilder.toString()
+        })
+
+        button_add_quote.setOnClickListener {
+            val quote = Quote(editText_quote.text.toString(), editText_author.text.toString())
+            viewModel.addQuotes(quote)
+            editText_quote.setText("")
+            editText_author.setText("")
+        }
     }
 }
